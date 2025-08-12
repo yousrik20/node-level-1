238b15db5288bba3b4e62a7fd90d4b548e1f0937
yousrik20 <yousri.k.e13@gmail.com>
2025-08-12 14:46:23 +0200

    Lesson 3

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
 
-app.get('/', (req, res) => {
-  res.sendFile("./views/home.html",{root:__dirname});
-})
+app.get("/", (req, res) => {
+  res.sendFile("./views/home.html", { root: __dirname });
+});
 
-app.listen(port, () => {
-  console.log(`http://localhost:${port}/`)
-})
+mongoose
+  .connect(
+    "mongodb+srv://yousrike13:121995@cluster0.zcpamik.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`http://localhost:${port}/`);
+    });
+  })
+  .catch((err) => {
+    console.log(err);
+  });
