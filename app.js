dee17f757aa7bca7ac38bd2cae7e14f3d39e4aec
yousrik20 <yousri.k.e13@gmail.com>
2025-08-12 15:52:16 +0200

    Lesson 4

--- app.js
+++ app.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const mongoose = require("mongoose");
+app.use(express.urlencoded({ extended: true }));
+const Mydata = require("./models/mydataSchema");
 
 app.get("/", (req, res) => {
   res.sendFile("./views/home.html", { root: __dirname });
 });
 
+app.get("/views/index.html", (req, res) => {
+  res.sendFile("./views/index.html", { root: __dirname });
+});
 mongoose
   .connect(
     "mongodb+srv://yousrike13:121995@cluster0.zcpamik.mongodb.net/all-data?retryWrites=true&w=majority&appName=Cluster0"
@@ -19,3 +24,13 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
+
+app.post("/", (req, res) => {
+  console.log(req.body);
+  const mydata = new Mydata(req.body);
+  mydata.save().then(() => {
+    res.redirect("./views/index.html");
+  }).catch((err)=>{
+    console.log(err);
+  })
+});
