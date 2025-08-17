b7095f2578a6e06de87f4c0c538dc95779325bf3
yousrik20 <yousri.k.e13@gmail.com>
2025-08-17 19:48:22 +0200

    Lesson 13

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-var moment = require('moment'); // require
+var moment = require("moment"); // require
+var methodOverride = require("method-override");
+app.use(methodOverride("_method"));
 
 // Auto Refresh
 const path = require("path");
@@ -28,26 +30,27 @@ liveReloadServer.server.once("connection", () => {
 app.get("/", (req, res) => {
   User.find()
     .then((result) => {
-      res.render("index", { arr: result ,moment:moment});
+      res.render("index", { arr: result, moment: moment });
     })
     .catch((err) => {
       console.log(err);
     });
 });
 
-
 app.get("/user/add.html", (req, res) => {
   res.render("user/add");
 });
 
-app.get("/user/edit.html", (req, res) => {
-  res.render("./user/edit");
+app.get("/edit/:id", (req, res) => {
+  User.findById(req.params.id).then((result)=>{
+    res.render("./user/edit",{obj:result,moment:moment});
+  })
 });
 
-app.get("/user/:id", (req, res) => {
+app.get("/view/:id", (req, res) => {
   User.findById(req.params.id)
     .then((result) => {
-      res.render("user/view", { obj: result ,moment:moment});
+      res.render("user/view", { obj: result, moment: moment });
     })
     .catch((err) => {
       console.log(err);
@@ -58,6 +61,18 @@ app.post("/user/add.html", (req, res) => {
   const user = new User(req.body);
   user
     .save()
+    .then(() => {
+      res.redirect("/");
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+// DELETE Request
+
+app.delete("/edit/:id", (req, res) => {
+  User.findByIdAndDelete(req.params.id)
     .then(() => {
       res.redirect("/");
     })
