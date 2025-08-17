0d046ef69f6e415b2845b9caf19ff92d2be78e49
yousrik20 <yousri.k.e13@gmail.com>
2025-08-17 18:16:12 +0200

    Lesson 12

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+var moment = require('moment'); // require
 
 // Auto Refresh
 const path = require("path");
@@ -27,30 +28,31 @@ liveReloadServer.server.once("connection", () => {
 app.get("/", (req, res) => {
   User.find()
     .then((result) => {
-      res.render("index", { arr: result });
+      res.render("index", { arr: result ,moment:moment});
     })
     .catch((err) => {
       console.log(err);
     });
 });
 
-app.get("/user/:id", (req, res) => {
-  User.findById(req.params.id)
-    .then((result) => {
-      res.render("user/view", { obj: result });
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-});
+
 app.get("/user/add.html", (req, res) => {
-  res.render("./user/add");
+  res.render("user/add");
 });
 
 app.get("/user/edit.html", (req, res) => {
   res.render("./user/edit");
 });
 
+app.get("/user/:id", (req, res) => {
+  User.findById(req.params.id)
+    .then((result) => {
+      res.render("user/view", { obj: result ,moment:moment});
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
 // POST Request
 app.post("/user/add.html", (req, res) => {
   const user = new User(req.body);
--- models/customerSchema.js
+++ models/customerSchema.js
@@ -1,23 +1,22 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-
 // define the Schema (the structure of the article)
-const userSchema = new Schema({
-  firstName: String,
-  lastName: String,
-  email: String,
-  phonenumber: String,
-  age: Number,
-  country: String,
-  gender: String,
-});
-
+const userSchema = new Schema(
+  {
+    firstName: String,
+    lastName: String,
+    email: String,
+    phonenumber: String,
+    age: Number,
+    country: String,
+    gender: String,
+  },
+  { timestamps: true }
+);
 
 // Create a model based on that schema
 const User = mongoose.model("customer", userSchema);
 
- 
- 
 // export the model
 module.exports = User;
