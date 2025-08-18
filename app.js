9aa99277dde9bc35aa882755637d24005a8ff274
yousrik20 <yousri.k.e13@gmail.com>
2025-08-18 12:43:44 +0200

    Lesson 16

--- app.js
+++ app.js
@@ -67,6 +67,18 @@ app.post("/user/add.html", (req, res) => {
     });
 });
 
+app.post("/search", (req, res) => {
+  console.log('***************************************')
+  User.find({$or:[{firstName:"ali"},{lastName:"ali"}]})
+    .then((result) => {
+      console.log(result);
+      res.render("user/search", { arr: result, moment: moment });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
 // DELETE Request
 
 app.delete("/edit/:id", (req, res) => {
